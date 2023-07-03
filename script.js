a9eb2c3cd26583e020275605175d4cb8ac96230b
Bwinecoor <123968562+Bwinecoor@users.noreply.github.com>
2023-07-03 12:15:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,113 @@
+function addMyClass() {
+    var button = document.getElementById("myButton");
+    button.classList.add("myClass");
+  }
+  function addMyClass1() {
+    var button = document.getElementById("myButton1");
+    button.classList.add("myClass1");
+  }
+  function addMyClass2() {
+    var button = document.getElementById("myButton2");
+    button.classList.add("myClass2");
+  }
+  function addMyClass3() {
+    var button = document.getElementById("myButton3");
+    button.classList.add("myClass3");
+  }
+  function addMyClass4() {
+    var button = document.getElementById("myButton4");
+    button.classList.add("myClass4");
+  }
+  function addMyClass5() {
+    var button = document.getElementById("myButton5");
+    button.classList.add("myClass5");
+  }
+  function addMyClass6() {
+    var button = document.getElementById("myButton6");
+    button.classList.add("myClass6");
+  }
+  function addMyClass7() {
+    var button = document.getElementById("myButton7");
+    button.classList.add("myClass7");
+  }
+  function addMyClass8() {
+    var button = document.getElementById("myButton8");
+    button.classList.add("myClass8");
+  }
+  function addMyClass9() {
+    var button = document.getElementById("myButton9");
+    button.classList.add("myClass9");
+  }
+  function addMyClass10() {
+    var button = document.getElementById("myButton10");
+    button.classList.add("myClass10");
+  }
+  function addMyClass11() {
+    var button = document.getElementById("myButton11");
+    button.classList.add("myClass11");
+  }
+  function addMyClass12() {
+    var button = document.getElementById("myButton12");
+    button.classList.add("myClass12");
+  }
+  function addMyClass13() {
+    var button = document.getElementById("myButton13");
+    button.classList.add("myClass13");
+  }
+  function addMyClass14() {
+    var button = document.getElementById("myButton14");
+    button.classList.add("myClass14");
+  }
+  function addMyClass15() {
+    var button = document.getElementById("myButton15");
+    button.classList.add("myClass15");
+  }
+  function addMyClass16() {
+    var button = document.getElementById("myButton16");
+    button.classList.add("myClass16");
+  }
+  function addMyClass17() {
+    var button = document.getElementById("myButton17");
+    button.classList.add("myClass17");
+  }
+  function addMyClass18() {
+    var button = document.getElementById("myButton18");
+    button.classList.add("myClass18");
+  }
+  function addMyClass19() {
+    var button = document.getElementById("myButton19");
+    button.classList.add("myClass19");
+  }
+  function addMyClass20() {
+    var button = document.getElementById("myButton20");
+    button.classList.add("myClass20");
+  }
+  function addMyClass21() {
+    var button = document.getElementById("myButton21");
+    button.classList.add("myClass21");
+  }
+  function addMyClass22() {
+    var button = document.getElementById("myButton22");
+    button.classList.add("myClass22");
+  }
+  function addMyClass23() {
+    var button = document.getElementById("myButton23");
+    button.classList.add("myClass23");
+  }
+  function addMyClass24() {
+    var button = document.getElementById("myButton24");
+    button.classList.add("myClass24");
+  }
+  function addMyClass25() {
+    var button = document.getElementById("myButton25");
+    button.classList.add("myClass25");
+  }
+  function addMyClass26() {
+    var button = document.getElementById("myButton26");
+    button.classList.add("myClass26");
+  }
+  function addMyClass27() {
+    var button = document.getElementById("myButton27");
+    button.classList.add("myClass27");
+  }
+  
